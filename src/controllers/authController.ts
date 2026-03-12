76d8f42937cc8b978ab1af4edeb254870f047c65
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-12 11:48:19 +0300

    feat: add login controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -10,7 +10,7 @@ interface RegisterBody {
 }
 
 
-const authRegister = async (
+export const authRegister = async (
     req: Request<{}, {}, RegisterBody>,
     res: Response
 
@@ -56,5 +56,56 @@ const authRegister = async (
 
     }
 
+}
 
+
+
+interface LoginBody {
+    email: string
+    password: string
+}
+
+
+
+export const authLogin = async (
+    req: Request<{}, {}, LoginBody>,
+    res: Response
+) => {
+
+    try {
+
+        const email = req.body.email
+        const password = req.body.password
+
+        const existingUser = await User.findOne({ email })
+
+        if (!existingUser) {
+            return res.status(400).json({ message: "Kullanıcı bulunamadı" })
+        }
+
+        const match = await bcrypt.compare(password, existingUser.password);
+
+        if (!match) {
+            return res.status(400).json({ message: "Sifre yanlis" });
+        }
+
+        const jwtSecretKey = process.env.JWT_SECRET
+
+        if (!jwtSecretKey) {
+            throw new Error("JWT_SECRET tanımlı değil")
+        }
+
+        const token = jwt.sign(
+            { id: existingUser._id },
+            jwtSecretKey,
+            { expiresIn: '1h' });
+
+
+        return res.status(200).json({ message: "Giris basarili", token });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Server hatası" })
+
+    }
 }
