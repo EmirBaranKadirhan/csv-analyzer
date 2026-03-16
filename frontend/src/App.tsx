8e61cb6da2e1fd4e31d408c84cdc02aee1bddc26
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-16 17:45:31 +0300

    feat: add Dashboard page and route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import AuthPage from "./pages/AuthPage"
 import { Routes, Route } from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Routes>
         <Route path="/" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   )
